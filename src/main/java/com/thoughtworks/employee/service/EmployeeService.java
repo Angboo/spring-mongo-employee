1733d2271ca32a5bcddea3ef6419115dc16928b8
Ang Zhang <anzhang@thoughtworks.com>
2020-12-02 18:13:41 +0800

    implement mongo repository

---
+++ src/main/java/com/thoughtworks/employee/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.employee.service;
+
+import com.thoughtworks.employee.entity.Employee;
+import com.thoughtworks.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+}
